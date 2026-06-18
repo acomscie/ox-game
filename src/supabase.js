4968f4caa336f0a0ecf29f9f04f2275cda9e50f6
acomscie <suthepa1983@outlook.com>
2026-06-18 15:54:37 +0700

    fix supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ekbfbmlpjiqevoudlngg.supabase.co";
-const supabaseKey = "sb_publishable_lTEyXiG_OeF6DhpYren58Q_D8wNLfcI";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrYmZibWxwamlxZXZvdWRsbmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2Nzc4NzUsImV4cCI6MjA5NzI1Mzg3NX0.QCgHMaD7YU724S6ef1zgRQAFZfU6s4gxq969ZOcNcAc";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
